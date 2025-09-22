0b0a980782521dda1096d828dd00953977e9aae2
lyx0903 <liuyaxing0903@163.com>
2025-09-22 23:49:54 +0800

    练习页面功能完善

--- src/views/demos/users/Dialog.vue
+++ src/views/demos/users/Dialog.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog v-model="dialogObj.show" :title="dialogObj.title" width="500">
-    <el-form model="dialogForm">
+    <el-form ref="dialogFormRef" :model="dialogForm" :rules="rules">
       <el-form-item label="ID" :label-width="formLabelWidth" prop="id">
         <el-input v-model="dialogForm.id" />
       </el-form-item>
@@ -26,8 +26,23 @@
           <el-option label="普通用户" value="user" />
         </el-select>
       </el-form-item>
+      <el-form-item
+        label="创建时间"
+        :label-width="formLabelWidth"
+        prop="createdAt"
+      >
+        <el-date-picker
+          v-model="dialogForm.createdAt"
+          type="date"
+          :size="size"
+          format="YYYY/MM/DD"
+          value-format="YYYY-MM-DD"
+        />
+      </el-form-item>
       <el-form-item>
-        <el-button type="info" plain>取消</el-button>
+        <el-button type="info" plain @click="dialogObj.show = false"
+          >取消</el-button
+        >
         <el-button type="primary" @click="submit">提交</el-button>
       </el-form-item>
     </el-form>
@@ -36,9 +51,11 @@
 
 <script setup>
 import axios from "axios";
-import { reactive } from "vue";
+import { ElMessage } from "element-plus";
+import { reactive, useTemplateRef } from "vue";
 
 const formLabelWidth = "80px";
+const dialogFormRef = useTemplateRef("dialogFormRef");
 
 const dialogObj = reactive({
   show: false,
@@ -52,25 +69,83 @@ const dialogForm = reactive({
   email: null,
   status: "",
   role: "",
+  createdAt: "",
 });
 
-const submit = () => {
-  axios
-    .post("/api/users", { ...dialogForm })
-    .then(function (res) {
-      // console.log(res);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      // 总是会执行
-    });
+// 弹窗验证
+const rules = reactive({
+  id: [{ required: true, message: "请输入id", trigger: "blur" }],
+  name: [
+    { required: true, message: "请输入姓名", trigger: "blur" },
+    { min: 2, max: 5, message: "Length should be 2 to 5", trigger: "blur" },
+  ],
+  phone: [
+    { required: true, message: "请输入联系电话", trigger: "blur" },
+    {
+      pattern: /^1[3-9]\d{9}$/,
+      message: "请输入有效的手机号",
+      trigger: "blur",
+    },
+  ],
+  email: [
+    { required: true, message: "请输入邮箱", trigger: "blur" },
+    {
+      pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      message: "请输入有效的邮箱地址",
+      trigger: "blur",
+    },
+  ],
+});
+
+// 提交
+const emits = defineEmits(["addOk"]);
+const submit = async () => {
+  const valid = await dialogFormRef.value.validate();
+  if (valid) {
+    if (dialogObj.title === "新增") {
+      axios
+        .post("/api/users", { ...dialogForm })
+        .then(function (res) {
+          emits("addOk"); // 成功事件返回emit
+          dialogObj.show = false;
+          ElMessage({ type: "success", message: "添加成功" });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(function () {
+          // 总是会执行
+        });
+    } else if (dialogObj.title === "编辑") {
+      axios
+        .put(`/api/users/${dialogForm.id}`, { ...dialogForm }) // url 和 发送到服务器的参数
+        .then(function (res) {
+          emits("addOk"); // 成功事件返回emit
+          dialogObj.show = false;
+          ElMessage({ type: "success", message: "修改成功" });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(function () {
+          // 总是会执行
+        });
+    }
+  } else {
+    console.log("error submit!", fields);
+  }
+};
+
+// 重置表单（form中需要添加 ref 属性，dialogFormRef是通过ref获取的表单组件引用 ）
+const resetForm = () => {
+  dialogFormRef.value.resetFields();
 };
 
 // 暴露子组件的属性给父组件（关键！否则父组件无法访问 dialogObj)
 defineExpose({
   dialogObj, // 将 dialogObj 暴露出去，父组件可通过 dialogRef.value.dialogObj 访问
+  dialogForm,
+  resetForm,
 });
 </script>
 
--- src/views/demos/users/users.vue
+++ src/views/demos/users/users.vue
@@ -22,21 +22,43 @@
     <Btns>
       <template #btns>
         <el-button type="primary" @click="openDialog('新增')">新增</el-button>
-        <el-button type="danger">批量删除</el-button>
+        <el-button
+          type="danger"
+          :disabled="multipleSelection.length === 0"
+          @click="batchDelete"
+          >批量删除</el-button
+        >
       </template>
     </Btns>
     <div class="user-table">
-      <el-table :data="tableData">
-        <el-table-column prop="id" label="id" />
-        <el-table-column prop="name" label="姓名" />
+      <el-table
+        :data="tableData"
+        ref="multipleTableRef"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="id" label="id" width="100" />
+        <el-table-column prop="name" label="姓名" width="100" />
         <el-table-column prop="phone" label="联系电话" />
         <el-table-column prop="email" label="邮箱" />
         <el-table-column prop="status" label="状态">
           <template #default="scope">
             <el-tag
-              :type="scope.row.status === 'enable' ? 'success' : 'danger'"
+              :type="
+                scope.row.status === 'enable'
+                  ? 'success'
+                  : scope.row.status === 'disable'
+                  ? 'danger'
+                  : 'info'
+              "
             >
-              {{ scope.row.status === "enable" ? "启用" : "禁用" }}
+              {{
+                scope.row.status === "enable"
+                  ? "启用"
+                  : scope.row.status === "disable"
+                  ? "禁用"
+                  : "-"
+              }}
             </el-tag>
           </template>
         </el-table-column>
@@ -47,7 +69,7 @@
               :type="
                 scope.row.role === 'admin'
                   ? 'primary'
-                  : scope.row.role === 'user'
+                  : scope.row.role === 'editor'
                   ? 'warning'
                   : 'info'
               "
@@ -55,32 +77,43 @@
               {{
                 scope.row.role === "admin"
                   ? "管理员"
-                  : scope.row.role === "user"
-                  ? "普通用户"
-                  : "编辑"
+                  : scope.row.role === "editor"
+                  ? "编辑"
+                  : "普通用户"
               }}
             </el-tag>
           </template>
         </el-table-column>
 
         <el-table-column prop="createdAt" label="创建时间" />
-        <el-table-column>
-          <template #default>
-            <el-button type="primary" plain>编辑</el-button>
-            <el-button type="danger" plain>删除</el-button>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button
+              type="primary"
+              :text="true"
+              @click="openDialog('编辑', scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              type="danger"
+              :text="true"
+              @click="deleteRow(scope.row.id)"
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <Dialog ref="dialogRef"></Dialog>
+    <Dialog ref="dialogRef" @addOk="addOk"></Dialog>
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive, ref, useTemplateRef } from "vue";
+import { onMounted, reactive, ref, useTemplateRef, nextTick } from "vue";
 import axios from "axios";
 import Btns from "./Btns.vue";
 import Dialog from "./Dialog.vue";
+import { ElMessage, ElMessageBox } from "element-plus";
 
 onMounted(() => {
   getUsersList();
@@ -94,8 +127,49 @@ const formInline = reactive({
 
 // 表格
 const tableData = ref([]); //动态数据
+// 删除行
+const deleteRow = async (id) => {
+  try {
+    const isDelete = await ElMessageBox.confirm("确定要删除该数据吗?", "警告", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning",
+    });
+    if (isDelete) {
+      await axios.delete(`/api/users/${id}`);
+      ElMessage({ type: "success", message: "删除成功" });
+      getUsersList();
+    }
+  } catch (error) {
+    ElMessage({ type: "info", message: "已取消" });
+  }
+};
 
-// 获取列表
+// 批量删除
+const multipleSelection = ref([]); //多选数据
+const handleSelectionChange = (val) => {
+  multipleSelection.value = val;
+};
+const batchDelete = async () => {
+  try {
+    const isDelete = await ElMessageBox.confirm("确定删除吗?", "警告", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning",
+    });
+    if (isDelete) {
+      for (const item of multipleSelection.value) {
+        await axios.delete(`/api/users/${item.id}`);
+      }
+      ElMessage({ type: "success", message: "删除成功" });
+      getUsersList();
+    }
+  } catch (error) {
+    ElMessage({ type: "info", message: "已取消" });
+  }
+};
+
+// 获取列表--请求接口
 function getUsersList() {
   axios
     .get("/api/users", { params: formInline })
@@ -116,7 +190,7 @@ const query = () => {
   getUsersList();
 };
 
-// 重置 -- 模板中要有 prop 才能重置成功
+// 重置 -- 模板中要有 prop 才能重置成功， prop 的 value 与绑定对象的 key 一致
 const formInlineRef = useTemplateRef("formInlineRef");
 
 const reset = () => {
@@ -126,11 +200,32 @@ const reset = () => {
 
 // 打开弹窗
 const dialogRef = useTemplateRef("dialogRef");
-// console.log(2, dialogRef.value);
 
-const openDialog = (flag,row) => {
-  dialogRef.value.dialogObj.show = true;
-  dialogRef.value.dialogObj.title = flag;
+const openDialog = async (flag, row) => {
+  dialogRef.value.dialogObj.show = true; //打开
+  dialogRef.value.dialogObj.title = flag; //标题赋值
+
+  // DOM 未更新
+  await nextTick();
+  // DOM 此时已经更新
+
+  //重置
+  dialogRef.value.resetForm();
+
+  if (flag === "新增") {
+  } else if (flag === "编辑") {
+    // let editObj = { ...row };
+    // dialogRef.value.dialogForm = { ...row };
+    // debugger
+    // console.log(row);
+    Object.assign(dialogRef.value.dialogForm, row); //数据回显
+    // console.log("赋值后的数据:", dialogRef.value.dialogForm);
+  }
+};
+
+// 弹窗新增成功传递的 emit 事件，父组件刷新列表
+const addOk = () => {
+  getUsersList();
 };
 </script>
 
