8d4830c6bfd4d3314eb8637407adbddec451bed0
lyx0903 <liuyaxing0903@163.com>
2025-10-24 17:10:04 +0800

    todolist

---
+++ src/views/todoList/addDialog.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dialog
+    v-model="addDialogObj.show"
+    :title="addDialogObj.title"
+    :width="addDialogObj.width"
+  >
+    <el-form :model="form">
+      <el-form-item label="Promotion name" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Zones" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="Please select a zone">
+          <el-option label="Zone No.1" value="shanghai" />
+          <el-option label="Zone No.2" value="beijing" />
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="addDialogObj.show = false">Cancel</el-button>
+        <el-button type="primary" @click="addDialogObj.show = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+
+const addDialogObj = reactive({
+  show: false,
+  title: "新增",
+  width: 500,
+});
+
+// const dialogFormVisible = ref(false);
+const formLabelWidth = "140px";
+
+const form = reactive({
+  name: "",
+  region: "",
+  date1: "",
+  date2: "",
+  delivery: false,
+  type: [],
+  resource: "",
+  desc: "",
+});
+
+// 暴露给父组件的属性和方法
+defineExpose({ addDialogObj });
+</script>
--- src/views/todoList/index.vue
+++ src/views/todoList/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="search-box">
-    <el-form :inline="true" :model="searchForm" class="demo-form-inline">
-      <el-form-item label="状态">
+    <el-form
+      :inline="true"
+      :model="searchForm"
+      class="demo-form-inline"
+      ref="searchFormRef"
+    >
+      <el-form-item label="状态" prop="status">
         <el-select v-model="searchForm.status" placeholder="状态" clearable>
           <el-option
             v-for="(item, index) in demandStatus"
@@ -10,58 +15,41 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="提出日期">
+      <el-form-item label="提出日期" prop="proposalDate">
         <el-date-picker
-          v-model="searchForm.date"
+          v-model="searchForm.proposalDate"
           type="date"
           placeholder="请选择"
           clearable
         />
       </el-form-item>
-      <el-form-item label="需求类型">
-        <el-select v-model="searchForm.type" placeholder="需求类型" clearable>
-          <el-option
-            v-for="(item, index) in demandType"
-            :key="index"
-            :value="item.value"
-          />
-        </el-select>
-      </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-        <el-button type="warning" @click="onSubmit">只看我的</el-button>
+        <el-button type="primary" @click="search">查询</el-button>
+        <el-button type="info" @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
   <div class="btns">
-    <el-button type="primary" @click="onSubmit">新增</el-button>
-    <el-button type="primary" @click="onSubmit">优化</el-button>
-    <el-button type="success" @click="onSubmit">导出</el-button>
-
-    <!-- <el-input
-      v-model="input2"
-      style="width: 240px"
-      placeholder="Please Input"
-      :suffix-icon="Search"
-    /> -->
+    <el-button type="primary" @click="openDialog('新增')">新增</el-button>
   </div>
   <div class="table">
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="pt" label="平台" />
-      <el-table-column prop="mk" label="模块" />
-      <el-table-column prop="gn" label="功能" />
-      <el-table-column prop="xqlx" label="需求类型" />
-      <el-table-column prop="xqmx" label="需求明细" />
-      <el-table-column prop="zt" label="状态" />
-      <el-table-column prop="tcr" label="提出人" />
-      <el-table-column prop="fzr" label="负责人" />
-      <el-table-column prop="qd" label="前端" />
-      <el-table-column prop="hd" label="后端" />
-      <el-table-column prop="qdgzl" label="前端工作量" />
-      <el-table-column prop="hdgzl" label="后端工作量" />
-      <el-table-column prop="tcrq" label="提出日期" />
-      <el-table-column prop="fj" label="附件" />
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :row-class-name="tableRowClassName"
+    >
+      <el-table-column prop="platform" label="平台" />
+      <el-table-column prop="module" label="模块" />
+      <el-table-column prop="feature" label="功能" width="200" />
+      <el-table-column prop="status" label="状态" />
+      <el-table-column prop="proposer" label="负责人" />
+      <el-table-column prop="frontend" label="前端" />
+      <el-table-column prop="backend" label="后端" />
+      <el-table-column prop="workload" label="工作量" />
+      <el-table-column prop="proposalDate" label="提出日期" />
+      <el-table-column prop="startDate" label="开始时间" />
+
       <el-table-column
         prop="address"
         fixed="right"
@@ -69,60 +57,137 @@
         min-width="200"
       >
         <template #default>
-          <el-button link type="primary" size="small">状态</el-button>
-          <el-button link type="primary" size="small">修改</el-button>
-          <el-button link type="primary" size="small">流转</el-button>
-          <el-button link type="danger" size="small">删除</el-button>
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="openDialog('状态变更')"
+            >状态</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="openDialog('修改')"
+            >修改</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="openDialog('流转')"
+            >流转</el-button
+          >
+          <el-button
+            link
+            type="danger"
+            size="small"
+            @click="deleteRow(scope.row.id)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
   </div>
+  <addDialog ref="addDialogRef"></addDialog>
 </template>
 <script setup>
-import { reactive, ref } from "vue";
+import { onMounted, reactive, ref, useTemplateRef } from "vue";
+import { ElMessage, ElMessageBox } from "element-plus";
+import axios from "axios";
+import addDialog from "./addDialog.vue";
 
-const searchForm = reactive({
-  status: "",
-  date: "",
-  type: "",
+// 页面初始化加载列表
+onMounted(() => {
+  getlist();
 });
 
+// 获取列表
+const getlist = () => {
+  axios({
+    method: "get",
+    url: "/api/todolist",
+    params: { ...searchForm },
+  })
+    .then((res) => {
+      console.log(res.data);
+      tableData.value = res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const searchForm = reactive({}); // 查询条件数据
+const tableData = ref([]); // 表格数据
+// 状态下拉框
 const demandStatus = reactive([
-  { label: "待审批", value: "待审批" },
+  { label: "待评审", value: "待评审" },
   { label: "待开发", value: "待开发" },
   { label: "开发中", value: "开发中" },
   { label: "待验收", value: "待验收" },
 ]);
-const demandType = reactive([
-  { label: "新增", value: "新增" },
-  { label: "优化", value: "优化" },
-]);
 
-const onSubmit = () => {
-  console.log("submit!");
+// 查询
+const search = () => {
+  getlist();
+};
+// 重置
+const searchFormRef = useTemplateRef("searchFormRef");
+const reset = () => {
+  searchFormRef.value.resetFields();
+  getlist();
+};
+// 删除
+const deleteRow = (id) => {
+  ElMessageBox.confirm("确认删除吗?", "Warning", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    .then(() => {
+      ElMessage({
+        type: "success",
+        message: "取消",
+      });
+    })
+    .catch((action) => {
+      ElMessage({
+        type: "info",
+        message: action === "cancel" ? "已取消" : "",
+      });
+    });
+  // try {
+  // } catch (error) {
+  //   ElMessage({ type: "info", message: "已取消" });
+  // }
 };
 
-const tableDataAll = ref([]);
-const tableData = [
-  {
-    pt: "赤菟",
-    mk: "需求",
-    gn: "代办",
-    xqlx: "新增",
-    xqmx: "123",
-    zt: "待开发",
-    tcr: "张三",
-    fzr: "李四",
-    qd: "wangwu",
-    hd: "zl",
-    qdgzl: "2",
-    hdgzl: "5",
-    tcrq: "2025-10-10",
-    fj: "",
-  },
-];
+// 打开弹窗
+const addDialogRef = useTemplateRef("addDialogRef");
+const openDialog = async (flag, row) => {
+  console.log(addDialogRef.value.addDialogObj);
 
-const input2 = ref("");
+  addDialogRef.value.addDialogObj.show = true;
+  addDialogRef.value.addDialogObj.title = flag;
+};
+
+// 表格状态
+const tableRowClassName = (row, rowIndex) => {
+  if (rowIndex === 1) {
+    return "warning-row";
+  } else if (rowIndex === 3) {
+    return "success-row";
+  }
+  return "";
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-table .warning-row {
+  --el-table-tr-bg-color: #ff0000;
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+</style>
